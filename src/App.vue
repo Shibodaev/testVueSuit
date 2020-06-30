cbe9b54d732ffb437376ac4ce09c848a65e3d2b3
Mike Shibodaev <mike756@mail.ru>
2020-07-01 00:43:34 +0400

    install package

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  components: {
+    
+    
+  }
+};
+</script>
+
+<style lang="scss" >
+// @import '~bootstrap';
+// @import '~bootstrap-vue';
+@import '~pixel-glass/styles.css';
+@import './assets/scss/fonts.css';
+
+HTML {
+background-repeat: no-repeat;
+background-position:  50% 0;
+/* Mobile layout by default */
+background-image: url( "./assets/320.png" );
+}
+/* Tablet */
+@media ( min-width: 760px ) {
+HTML {
+    /* Tablet layout */
+    background-image: url( "./assets/760.png" );
+}
+}
+/* Desktop */
+@media ( min-width: 960px ) {
+HTML {
+    /* Desktop layout */
+    background-image: url( "./assets/960.png" );
+}
+}
+
+#app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+//   margin-top: 60px;
+//   background-color: $this_red_color;
+  font-family: "DINPro-Medium";
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+// const styleResourcesLoader = require('style-resources-loader');
+
+module.exports = {
+    pluginOptions: {
+        'style-resources-loader': {
+            'preProcessor': 'scss',
+            'patterns': [
+                path.resolve(__dirname, './src/assets/scss/style.scss'),
+                // path.resolve(__dirname, './src/assets/scss/fonts.css')
+                // path.resolve(__dirname, 'node_modules/bootstrap-vue/src/index.scss'),
+            ]
+        }
+    }
+}
